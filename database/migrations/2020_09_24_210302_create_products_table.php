1599dd9900411e57a3f90e63ac7fcca92a7ef2c8
Afolayan Oluwaseun Raphael <theafolayan@gmail.com>
2020-09-28 19:14:01 +0100

    fix: corrected  typo on create_products_table migration

--- database/migrations/2020_09_24_210302_create_products_table.php
+++ database/migrations/2020_09_24_210302_create_products_table.php
@@ -19,7 +19,7 @@ class CreateProductsTable extends Migration
             $table->string('image');
             $table->string('name');
             $table->integer('quantity');
-            $table->tex('description');
+            $table->text('description');
             $table->timestamps();
         });
     }
