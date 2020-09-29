33f1e6f8d717ada43b51872f93fcc0cee83625f0
Afolayan Oluwaseun Raphael <theafolayan@gmail.com>
2020-09-29 22:58:18 +0100

    feat: added description, price and quantity feilds to the create.blade.php view file for products

--- database/migrations/2020_09_24_210302_create_products_table.php
+++ database/migrations/2020_09_24_210302_create_products_table.php
@@ -15,9 +15,9 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->integer('price');
             $table->string('image');
-            $table->string('name');
             $table->integer('quantity');
             $table->text('description');
             $table->timestamps();
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,21 +1,33 @@
 @extends('layouts.app')
 @section('content')
 <div class="row">
-    <div class="col-md-8">
-        <div class ="card">
-    <div class= "card-header"> Add a New Product to Inventory
-    </div>
-    <div class= "card-body">
-        <div class="container">
-            <form action="{{ route('products.store') }}" method="post">
-            <div class="form-group">
-                <label for="name">Name</label>
-                <input type="text" class="form-control" placeholder ="Product Name here...">
+    <div class="col-md-4"></div>
+    <div class="col-md-4">
+        <div class="card">
+            <div class= "card-header"> Add a New Product to Inventory</div>
+            <div class= "card-body">
+                <div class="container">
+                    <form action="{{ route('products.store') }}" method="post">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" class="form-control" placeholder ="Product Name here...">
+                        </div>
+                        <div class="form-group">
+                            <label for="price">Price</label>
+                            <input type="number" class="form-control" name="price" placeholder ="Product price">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Quantity</label>
+                            <input type="number" name="quantity" class="form-control" placeholder ="ex 40...">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Description</label>
+                            <textarea type="text" class="form-control" placeholder ="Product description here..."> </textarea>
+                        </div>
+                    </form>
+                </div>
             </div>
-            </form>
         </div>
-    </div>
-</div>
     </div>
 </div>
 @endsection
